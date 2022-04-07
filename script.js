619ae1899b886c3b1b4ce616d69df357d90e7d84
Daianne Cordeiro <daianne.nc@gmail.com>
2022-04-07 03:38:39 -0300

    Super Trunfo desenvolvido na Imersão Dev

---
+++ script.js
@@ -0,0 +1,625 @@
+var carta1 = {
+  nome: "The Windy",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/0/04/Windy.png",
+  atributos: {
+    ataque: 7,
+    defesa: 7,
+    magia: 6
+}
+};
+
+var carta2 = {
+  nome: "The Fly",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/d/dd/Thefly_1wand.png",
+  atributos: {
+    ataque: 3,
+    defesa: 8,
+    magia: 4
+}
+};
+
+var carta3 = {
+  nome: "The Shadow",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/1/13/ShadowsSakura.png",
+  atributos: {
+    ataque: 6,
+    defesa: 2,
+    magia: 4
+}
+};
+
+var carta4 = {
+  nome: "The Watery",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/b/bf/811937-5209127-ccs_sealed_watery.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 6,
+    magia:  7
+}
+};
+
+var carta5 = {
+  nome: "The Rain",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/b/b2/Rain_4.JPG",
+  atributos: {
+    ataque: 3,
+    defesa: 5,
+    magia: 2
+}
+};
+
+var carta6 = {
+  nome: "The Wood",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/1/1d/Episode_4_-_Wood_Card_Full_Body.png",
+  atributos: {
+    ataque: 5,
+    defesa: 8,
+    magia: 6
+}
+};
+
+var carta7 = {
+  nome: "The Jump",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/3/3d/TheJump.png",
+  atributos: {
+    ataque: 7,
+    defesa: 9,
+    magia: 2
+}
+};
+
+var carta8 = {
+  nome: "The Illusion",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/d/d3/Episode_9.png",
+  atributos: {
+    ataque: 3,
+    defesa: 7,
+    magia: 7
+}
+};
+
+var carta9 = {
+  nome: "The Silent",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/1/13/Silent.jpg",
+  atributos: {
+    ataque: 2,
+    defesa: 5,
+    magia: 6
+}
+};
+
+var carta10 = {
+  nome: "The Thunder",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/3/36/Thunder.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 6,
+    magia: 8
+}
+};
+
+var carta11 = {
+  nome: "The Sword",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/8/89/Sword.png",
+  atributos: {
+    ataque: 8,
+    defesa: 5,
+    magia: 2
+}
+};
+
+var carta12 = {
+  nome: "The Flower",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/a/aa/Flower2.png",
+  atributos: {
+    ataque: 2,
+    defesa: 5,
+    magia: 7
+}
+};
+
+var carta13 = {
+  nome: "The Shield",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/4/43/Shield.jpg",
+  atributos: {
+    ataque: 4,
+    defesa: 10,
+    magia: 7
+}
+};
+
+var carta14 = {
+  nome: "The Time",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/b/bd/Episode_12.png",
+  atributos: {
+    ataque: 6,
+    defesa: 8,
+    magia: 10
+}
+};
+
+var carta15 = {
+  nome: "The Power",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/a/a3/Power.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 5,
+    magia: 4
+}
+};
+
+var carta16 = {
+  nome: "The Mist",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/6/6f/MistEp61.png",
+  atributos: {
+    ataque: 9,
+    defesa: 7,
+    magia: 6
+}
+};
+
+var carta17 = {
+  nome: "The Storm",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/4/48/Storm_true.png/",
+  atributos: {
+    ataque: 9,
+    defesa: 8,
+    magia: 7
+}
+};
+
+var carta18 = {
+  nome: "The Float",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/e/ea/Float.jpg",
+  atributos: {
+    ataque: 3,
+    defesa: 5,
+    magia:  6
+}
+};
+
+var carta19 = {
+  nome: "The Erase",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/a/a9/The-erase-caorrd-cardcapt.gif",
+  atributos: {
+    ataque: 7,
+    defesa: 7,
+    magia: 8
+}
+};
+
+var carta20 = {
+  nome: "The Glow",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/9/9a/Glow_Henshin.gif",
+  atributos: {
+    ataque: 2,
+    defesa: 2,
+    magia: 6
+}
+};
+
+var carta21 = {
+  nome: "The Move",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/1/11/Move.jpg",
+  atributos: {
+    ataque: 2,
+    defesa: 7,
+    magia: 6
+}
+};
+
+var carta22 = {
+  nome: "The Fight",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/5/5a/Fightmeiling.png",
+  atributos: {
+    ataque: 9,
+    defesa: 6,
+    magia: 2
+}
+};
+
+var carta23 = {
+  nome: "The Loop",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/b/b1/Loop_sealing.jpg",
+  atributos: {
+    ataque: 4,
+    defesa: 7,
+    magia: 6
+}
+};
+
+var carta24 = {
+  nome: "The Sleep",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/9/93/Sleep.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 8,
+    magia: 7
+}
+};
+
+var carta25 = {
+  nome: "The Song",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/a/a4/Ima49ge.jpg",
+  atributos: {
+    ataque: 1,
+    defesa: 3,
+    magia:  5
+}
+};
+
+var carta26 = {
+  nome: "The Little",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/d/de/Kero%26cake.png",
+  atributos: {
+    ataque: 5,
+    defesa: 6,
+    magia: 6
+}
+};
+
+var carta27 = {
+  nome: "The Mirror",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/7/79/Mirror1ep39.png",
+  atributos: {
+    ataque: 4,
+    defesa: 6,
+    magia: 8
+}
+};
+
+var carta28 = {
+  nome: "The Maze",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/9/91/Maze.png",
+  atributos: {
+    ataque: 6,
+    defesa: 8,
+    magia: 6
+}
+};
+
+var carta29 = {
+  nome: "The Return",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/6/6f/Return.png",
+  atributos: {
+    ataque: 3,
+    defesa: 2,
+    magia: 8
+}
+};
+
+var carta30 = {
+  nome: "The Shot",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/0/0f/Shot.jpg/",
+  atributos: {
+    ataque: 9,
+    defesa: 7,
+    magia: 3
+}
+};
+
+var carta31 = {
+  nome: "The Sweet",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/4/4a/Sweet.JPG",
+  atributos: {
+    ataque: 2,
+    defesa: 2,
+    magia: 5
+}
+};
+
+var carta32 = {
+  nome: "The Dash",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/2/25/Dash4.png",
+  atributos: {
+    ataque: 5,
+    defesa: 8,
+    magia: 6
+}
+};
+
+var carta33 = {
+  nome: "The Big",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/3/32/Bigsak.JPG",
+  atributos: {
+    ataque: 8,
+    defesa: 7,
+    magia: 5
+}
+};
+
+var carta34 = {
+  nome: "The Create",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/5/54/31_-_Sakura_And_The_Nameless_Book.mkv0010.png",
+  atributos: {
+    ataque: 8,
+    defesa: 8,
+    magia: 9
+}
+};
+
+var carta35 = {
+  nome: "The Change",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/8/8a/Capture.png",
+  atributos: {
+    ataque: 4,
+    defesa: 8,
+    magia: 6
+}
+};
+
+var carta36 = {
+  nome: "The Freeze",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/8/8b/483.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 8,
+    magia: 6
+}
+};
+
+var carta37 = {
+  nome: "The Firey",
+  imagem: "https://static.wikia.nocookie.net/",
+  atributos: {
+    ataque: 9,
+    defesa: 5,
+    magia: 6
+}
+};
+
+var carta38 = {
+  nome: "The Arrow",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/c/c3/Cheerio%21_2_052.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 5,
+    magia: 3
+}
+};
+
+var carta39 = {
+  nome: "The Bubbles",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/c/c2/Bubble.jpg",
+  atributos: {
+    ataque: 3,
+    defesa: 2,
+    magia: 4
+}
+};
+
+var carta40 = {
+  nome: "The Wave",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/1/17/ClowWave.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 5,
+    magia: 6
+}
+};
+
+var carta41 = {
+  nome: "The Through",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/d/d0/ClowThrough.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 8,
+    magia: 6
+}
+};
+
+var carta42 = {
+  nome: "The Libra",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/7/75/ClowLibra.jpg",
+  atributos: {
+    ataque: 6,
+    defesa: 6,
+    magia: 8
+}
+};
+
+var carta43 = {
+  nome: "The Snow",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/7/71/Snowcard.png",
+  atributos: {
+    ataque: 7,
+    defesa: 6,
+    magia: 7
+}
+};
+
+var carta44 = {
+  nome: "The Voice",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/0/05/Voice32.png",
+  atributos: {
+    ataque: 5,
+    defesa: 5,
+    magia: 7
+}
+};
+var carta45 = {
+  nome: "The Lock",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/f/fa/Th_Lock_Clow_Card.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 9,
+    magia: 6
+}
+};
+var carta46 = {
+  nome: "The Cloud",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/9/94/Cloud.jpg",
+  atributos: {
+    ataque: 4,
+    defesa: 5,
+    magia: 5
+}
+};
+var carta47 = {
+  nome: "The Dream",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/6/60/Episode_40.png",
+  atributos: {
+    ataque: 2,
+    defesa: 5,
+    magia: 9
+}
+};
+var carta48 = {
+  nome: "The Sand",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/8/89/SSSand.png",
+  atributos: {
+    ataque: 7,
+    defesa: 5,
+    magia: 5
+}
+};
+
+var carta49 = {
+  nome: "The Dark",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/2/2d/Episode_42_-_The_Light_and_The_Dark_Reunite_3.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 5,
+    magia: 9
+}
+};
+var carta50 = {
+  nome: "The Light",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/a/a9/Episode_42_-_Light_Card_01.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 7,
+    magia: 9
+}
+};
+
+var carta51 = {
+  nome: "The Twin",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/7/77/Thetwins_mainbody.png",
+  atributos: {
+    ataque: 7,
+    defesa: 5,
+    magia: 4
+}
+};
+
+var carta52 = {
+  nome: "The Earthy",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/7/7e/Theearthy_capture.png",
+  atributos: {
+    ataque: 8,
+    defesa: 7,
+    magia: 7
+}
+};
+
+var carta53 = {
+  nome: "The Nothing",
+  imagem: "https://static.wikia.nocookie.net/ccs/images/9/93/The_Nothing%27s_Main_Body.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 10,
+    magia: 10
+}
+};
+
+var cartas = [carta1,carta2,carta3,carta4,carta5,carta6,carta7,carta8,carta9,carta10,carta11,carta12,carta13,carta14,carta15,carta16,carta17,carta18,carta19,carta20,carta21,carta22,carta23,carta24,carta25,carta26,carta27,carta28,carta29,carta30,carta31,carta32,carta33,carta34,carta35,carta36,carta37,carta38,carta39,carta40,carta41,carta42,carta43,carta44,carta45,carta46,carta47,carta48,carta49,carta50,carta51,carta52,carta53];
+
+var cartaMaquina = 0;
+var cartaJogador = 0;
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * 53);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  var numeroCartaJogador = parseInt(Math.random() * 53);
+  while (numeroCartaJogador == numeroCartaMaquina) {
+    numeroCartaJogador = parseInt(Math.random() * 53);
+  }
+  cartaJogador = cartas[numeroCartaJogador];
+  console.log(cartaJogador);
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributo = document.getElementsByName("atributo");
+  for (var i = 0; i < radioAtributo.length; i++) {
+    if (radioAtributo[i].checked) {
+      return radioAtributo[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var divResultado = document.getElementById("resultado");
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>Carta Capturada!</p>";
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>A carta Clow é forte demais! Tente de novo!</p>";
+  } else {
+    htmlResultado = "<p class='resultado-final'>Quase lá, use uma carta mais forte!</p>";
+  }
+  divResultado.innerHTML = htmlResultado;
+
+  document.getElementById("btnJogar").disabled = true;
+  exibirCartaMaquina();
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+  var moldura =
+    '<img src="https://i.imgur.com/bgYsZfm.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+  var moldura =
+    '<img src="https://i.imgur.com/bgYsZfm.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+};
